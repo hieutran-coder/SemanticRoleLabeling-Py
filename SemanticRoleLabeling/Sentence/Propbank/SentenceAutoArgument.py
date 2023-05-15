18001a932d1adbbd95bfd60b965f217272c5f159
Trần Hữu Hiếu <57060034+hieutran-coder@users.noreply.github.com>
2023-05-15 15:54:05 +0700

    Update SentenceAutoArgument.py

--- SemanticRoleLabeling/Sentence/Propbank/SentenceAutoArgument.py
+++ SemanticRoleLabeling/Sentence/Propbank/SentenceAutoArgument.py
@@ -1,8 +1,12 @@
+
+# Có 2 module được import vào
+
 from abc import abstractmethod
 
 from AnnotatedSentence.AnnotatedSentence import AnnotatedSentence
 
-
+# Class SentenceAutoArgument thì không có Property Và có một abstract method
+    # Chữ kí của Abstract method này có seft parameter, và điều này fit với quan điểm trên W3school
 class SentenceAutoArgument:
 
     
